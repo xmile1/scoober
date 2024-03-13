7af0fd36b428c800ec026b73b8b6d7b3aac34f99
Uyiosa <hiskonxeptz@gmail.com>
2024-03-13 19:25:35 +0100

    feat: add join room feature

--- app/src/pages/Rooms/Rooms.tsx
+++ app/src/pages/Rooms/Rooms.tsx
@@ -2,6 +2,9 @@ import { Page } from "@/common/components";
 import { getRooms } from "@/common/services/api/rooms";
 import { socket } from "@/common/services/api/socket";
 import { RoomChooser } from "@/modules/RoomChooser";
+import { useCallback, useEffect, useState } from "react";
+import { GameOver } from "@/modules/GameOver";
+import { RoomsWrapper } from "./Rooms.styles";
 import { useEffect, useState } from "react";
 import { Room } from "@/common/models/room";
 
@@ -35,12 +38,18 @@ export const Rooms = () => {
     login();
   }, []);
 
+  const joinRoom = useCallback((room: Room) => {
+      socket.emit("joinRoom", { room: room.id, username: username, roomType: room.type });
+      setCurrentRoom(room);
+    },
+    [username]
+  );
+
+
   return (
     <Page>
       <RoomsWrapper>
-        <RoomChooser rooms={rooms} currentRoom={currentRoom} onRoomClick={()=>{}} />
-        <div/>
-
+        <RoomChooser rooms={rooms} currentRoom={currentRoom} onRoomClick={joinRoom} />
       </RoomsWrapper>
     </Page>
   );
